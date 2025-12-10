9dcca3a6abd1b7b3c6adbd4c400280c3edc3b5f0
gwenchii <mynameissteph21@outlook.com>
2025-12-11 06:19:32 +0800

    update rt model

--- retrain_model.py
+++ retrain_model.py
@@ -4,30 +4,25 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 import joblib
 
-# Load CSV
+
 data = pd.read_csv(r"C:\Users\mynam\Downloads\Clause-Level-Sentiment-Analysis\Final_Annotation.csv")
 
-# Strip whitespace from column names
+
 data.columns = data.columns.str.strip()
 print("Columns in CSV:", data.columns)
 
-# Drop any rows with missing values in relevant columns
 data = data.dropna(subset=['Clause', 'Final Sentiment'])
 
-# Features and labels
-X = data['Clause']             # text column
-y = data['Final Sentiment']    # label column
+X = data['Clause']
+y = data['Final Sentiment']
 
-# Build pipeline
 pipeline = Pipeline([
     ('vectorizer', CountVectorizer()),
     ('classifier', MultinomialNB())
 ])
 
-# Train model
 pipeline.fit(X, y)
 print("Model trained successfully!")
 
-# Save the model
 joblib.dump(pipeline, r"C:\Users\mynam\Downloads\Clause-Level-Sentiment-Analysis\models\taglish_sentiment_model.pkl")
 print("Model saved to 'models/taglish_sentiment_model.pkl'")
